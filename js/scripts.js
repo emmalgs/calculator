5b6d2baf68c765c52a8ca17f974a89000fe57888
Emma Gerigscott <elgerig@gmail.com>
2023-03-20 14:13:56 -0700

    subtract divide multiply functions and ui
    Co-authored-by: Kymani Stephens <krs00@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,43 @@
 // business logic
+const calcBtn = document.querySelectorAll(".calc-btn")
+const screen = document.querySelector(".calc-screen")
+console.log(calcBtn)
+
+calcBtn.addEventListener('click', display);
+
+function display() {
+    screen.innerHTML = `${calcBtn.value}`
+}
+
 function add(number1, number2) {
     return number1 + number2;
 }
 
-// user interface logic
-const number1 = prompt("Enter a number:");
-const number2 = prompt("Enter another number:");
-
-function checkNumber(number1, number2) {
-    if (number1 == parseInt(number1) && number2 == parseInt(number2)) {
-        return window.alert(add(parseInt(number1), parseInt(number2)));
-    } else {
-        return window.alert("Please enter a number");
-    }
+function subtract(number1, number2) {
+    return number1 - number2;
 }
 
-checkNumber(number1, number2)
+function divide(number1,number2) {
+    return number1 / number2;
+}
 
+function multiply(number1,number2) {
+    return number1 * number2;
+}
+
+// user interface logic
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// window.alert("The addition of " + number1 + " and " + number2 + " is:  " + add(number1, number2)
+//     + "\nThe subtraction of " + number1 + " and " + number2 + " is:  " + 
+//     subtract(number1, number2) + "\nThe division of " + number1 + " and " + number2 + " is:  " + divide(number1, number2)
+//     + "\nThe multiplication of " + number1 + " and " + number2 + " is:  " + multiply(number1, number2))
+
+
+
+// window.alert("The subtraction of the two numbers is " + subtract(number1, number2));
+// window.alert("The division of the two numbers is " + divide(number1, number2));
+// window.alert("The multiplication of the two numbers is " + multiply(number1, number2));
 
 
 
