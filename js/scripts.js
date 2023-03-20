d4c53072e00ad00ce1eface552ba70564b212876
Emma Gerigscott <elgerig@gmail.com>
2023-03-20 16:09:40 -0700

    WIP: still no worky
    Co-authored-by: Kymani Stephens <krs00@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,31 +4,34 @@ const opt = document.querySelectorAll(".opt")
 const screen = document.querySelector(".calc-screen")
 const clearBtn = document.querySelector(".clear")
 
-let num1 = '';
-let operator;
-let num2 = '';
+
+
 // makes button value appear on calc-screen
 calcBtn.forEach(btn => btn.addEventListener('click', () => {
+    let num1;
+    let operator;
+    let num2; 
+    let answer;
     if (btn.value == parseInt(btn.value)) {
         screen.innerHTML += `${btn.value}`;
-        num1 = parseInt(screen.innerHTML);
-        console.log(num1)
-    } else if (btn.value == '+' || btn.value == '-' || btn.value == '/' || btn.value == '*') {
+    } 
+    else if (btn.value == '+' || btn.value == '-' || btn.value == '/' || btn.value == '*') {
+        num1 = parseInt(screen.innerHTML)
         screen.innerHTML = '';
         operator = btn.value;
+        console.log(num1)
         console.log(operator)
-    } else if (btn.value == parseInt(btn.value)) {
-        screen.innerHTML += `${btn.value}`;
-        num2 = parseInt(screen.innerHTML)
+    } else {
+        num2 = parseInt(screen.innerHTML);
         console.log(num2)
-    } else if (btn.value == "=") {
         if (operator == "+") {
-            console.log(`num1 is equal to ${num1}`)
-            console.log(`num2 is equal to ${num2}`)
-            screen.innerHTML = add(num1,num2)
-            console.log(add(num1, num2))
-        }
+            return answer = add(num1,num2);
+        } else if (operator == '-') {
+            return answer = subtract(num1,num2)
+        } else {console.log(answer)}
+        
     }
+    
 }));
 
 
